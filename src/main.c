a2a7a95978184dc82037d00615493656b6aaf3db
Raph-Rodrigues <raph.edits06@gmail.com>
2026-07-21 23:57:01 -0300

    Fix: remaking the order of the events for creation of the window and adding a basic rendering with change buffers for the desktops linux with wayland compositor

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-#include <SDL2/SDL_events.h>
 #include <glad/glad.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -7,27 +6,43 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char *argv[]) {
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-
+  // Inicializa o subsistema de video do SDL
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("ERRO: nao pode iniciar SDL: %s\n", SDL_GetError());
     exit(1);
   }
 
+  // Configura os atributos do OpenGL após SDL_Init
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+  // Cria a janela
   SDL_Window *window =
       SDL_CreateWindow("Shooter Platformer", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
 
   if (!window) {
     printf("ERRO: não pode criar janela: %s\n", SDL_GetError());
+    SDL_Quit();
     exit(1);
   }
 
-  SDL_GL_CreateContext(window);
+  // salva o contexto na variável e verifica se houve erro
+  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+  if (!gl_context) {
+    printf("ERRO: não pode criar o contexto OpenGL: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    exit(1);
+  }
+
+  // Carrega os ponteiros do OpenGL usando glad
   if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
     printf("ERRO: não pode carregar o GL: %s\n", SDL_GetError());
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     exit(1);
   }
 
@@ -37,9 +52,12 @@ int main(int argc, char *argv[]) {
   printf("Version:   %s\n", glGetString(GL_VERSION));
 
   bool should_quit = false;
+
+  // loop principal
   while (!should_quit) {
     SDL_Event event;
 
+    // processamento de eventos
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
       case SDL_QUIT:
@@ -49,7 +67,21 @@ int main(int argc, char *argv[]) {
         break;
       }
     }
+
+    // renderização básica
+    // define a cor de fundo (cinza/azul escuro)
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // limpa o buffer de cor
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // troca os buffers, enviando a imagem gerada para a janela (BUGFIX para
+    // Linux Desktops com Wayland)
+    SDL_GL_SwapWindow(window);
   }
+  // Finalização e limpeza de memória
+  SDL_GL_DeleteContext(gl_context);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
   return 0;
 }
